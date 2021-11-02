24f38cbede6484fa1cd0bb3356eacb5a4069fbce
Maksim_V <maxivit1996@gmail.com>
2021-11-02 13:09:06 +0200

    fixed view topProduct

--- components/contectMain/topProductsSlider.jsx
+++ components/contectMain/topProductsSlider.jsx
@@ -3,29 +3,59 @@ import React from 'react'
 import stl from '../../styles/sliderStyles/topProductsSlider.module.scss'
 import CarouselWithButtons from '../../utility/Carousels/CarouselWithButtons';
 import Link from "next/link";
+import classNames from 'classnames';
 
 const TopProductsSlider = (props) => {
     return (
         <div className={stl.sectionContainer}>
-                <div className={stl.topTextContainer}>
-                    <span className={stl.topText}>
-                        <div><span>Топ </span>покупаемых товаров</div>
-                        <p>выбери то, что ближе тебе</p>
-                    </span>
-                </div>
-                <CarouselWithButtons>
-                    {props.popular.map(item => {
-                        return  <Link href="/product/[id]" as={`/product/${item.id}`} >
-                            <div className={stl.topItemContainer}>
-                                <Avatar variant="square" className={stl.image} src={`${process.env.SERVER_UPLOAD_URL}/${item.image}`} alt="item" />
-                                <span><p>{item.text}</p></span>
-                            </div>
-                    </Link>
-                    })}
-                </CarouselWithButtons>
-                <Link href="/products" ><button className={stl.btnBottom}>смотреть все товары</button></Link>
+            <div className={stl.topTextContainer}>
+                <span className={stl.topText}>
+                    <div><span>Топ </span>покупаемых товаров</div>
+                    <p>выбери то, что ближе тебе</p>
+                </span>
             </div>
-            )
+            {props.popular && props.popular.length > 2
+                ? TopSlider(props.popular)
+                : TopCard(props.popular)
+            }
+
+        </div>
+    )
 }
 
-            export default TopProductsSlider
+const TopSlider = (popular) => {
+    return (
+        <>
+            <CarouselWithButtons>
+                {popular.map(item => {
+                    return <Link href="/product/[id]" as={`/product/${item.id}`} >
+                        <div className={stl.topItemContainer}>
+                            <Avatar variant="square" className={stl.image} src={`${process.env.SERVER_UPLOAD_URL}/${item.image}`} alt="item" />
+                            <span><p>{item.text}</p></span>
+                        </div>
+                    </Link>
+                })}
+            </CarouselWithButtons>
+            <Link href="/products" ><button className={stl.btnBottom}>смотреть все товары</button></Link>
+        </>
+    )
+}
+
+const TopCard = (popular) => {
+    return (
+        <div className={classNames(stl.TopCardContainer, {[`${stl.TopCardItemOne}`]: popular.length === 1})}>
+            {popular.map(item => {
+                return <Link href="/product/[id]" as={`/product/${item.id}`} >
+                    <div className={stl.topItemContainer}>
+                        <Avatar variant="square" className={stl.image} src={`${process.env.SERVER_UPLOAD_URL}/${item.image}`} alt="item" />
+                        <span><p>{item.text}</p></span>
+                    </div>
+                </Link>
+            })}
+            <Link href="/products" ><button className={stl.btnBottom}>смотреть все товары</button></Link>
+        </div>
+    )
+}
+
+
+export default TopProductsSlider
